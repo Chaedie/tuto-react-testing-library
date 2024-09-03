ba83c626aef3c61a06d93cf75d4ec95ee1f7f3c1
ChaedongIm <chaedong.im.dev@gmail.com>
2024-09-03 12:48:11 +0900

    chore: config verbose to true for test name display

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -13,15 +13,18 @@ test("Greet renders correctly", () => {
  * Greet should render the text hello and if a name is passed into the component
  * It should render hello followed by the name
  */
-
-test("Greet renders correctly (TDD)", () => {
-  render(<Greet></Greet>);
-  const textElement = screen.getByText("Hello");
-  expect(textElement).toBeInTheDocument();
+describe("Greet", () => {
+  test("Greet renders correctly (TDD)", () => {
+    render(<Greet></Greet>);
+    const textElement = screen.getByText("Hello");
+    expect(textElement).toBeInTheDocument();
+  });
 });
 
-test("Greet renders with a name", () => {
-  render(<Greet name={"Chaedong"} />);
-  const textElement = screen.getByText("Hello Chaedong");
-  expect(textElement).toBeInTheDocument();
+describe("Nested", () => {
+  test("Greet renders with a name", () => {
+    render(<Greet name={"Chaedong"} />);
+    const textElement = screen.getByText("Hello Chaedong");
+    expect(textElement).toBeInTheDocument();
+  });
 });
