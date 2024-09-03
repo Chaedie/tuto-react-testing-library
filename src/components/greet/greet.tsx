d3b0520977f879b0fa2f73d1af1a1eeac25b496b
ChaedongIm <chaedong.im.dev@gmail.com>
2024-09-03 14:23:40 +0900

    hello

--- src/components/greet/greet.tsx
+++ src/components/greet/greet.tsx
@@ -3,7 +3,7 @@ import { GreetProps } from "./greet.types";
 export default function Greet(props: GreetProps) {
   return (
     <div>
-      <div>Hello {props.name}</div>
+      <div>Hello {props.name ? props.name : "Guest"}</div>
     </div>
   );
 }
