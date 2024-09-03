e472e9fda67a505a4e75807282f15332bbcf7ad9
ChaedongIm <chaedong.im.dev@gmail.com>
2024-09-03 13:37:31 +0900

    test: config CollectCoverageFrom

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,14 @@
-module.exports = {
+/** @type {import('jest').Config} */
+const config = {
   verbose: true,
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/components/**/*.{js,ts,tsx}", // Include all components
+    "!src/components/**/*.{types,stories,constants,test,spec}.{js,ts,tsx}", // Exclude test files
+    "!src/index.tsx", // Exclude the index file
+    "!src/reportWebVitals.ts", // Exclude the reportWebVitals file
+    "!src/**/*.d.ts", // Exclude TypeScript declaration files
+  ],
 };
+
+module.exports = config;
--- src/components/greet/greet.tsx
+++ src/components/greet/greet.tsx
@@ -1,6 +1,4 @@
-interface GreetProps {
-  name?: string;
-}
+import { GreetProps } from "./greet.types";
 
 export default function Greet(props: GreetProps) {
   return (
